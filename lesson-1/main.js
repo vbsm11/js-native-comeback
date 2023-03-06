cec82f5240ed2f76c58f63c57ca718c0f5635f62
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-06 23:24:57 +0300

    1 lesson

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,72 +1,98 @@
-// const user = {  // #4567 (адрес объекта в ОПБ)
-//     name: "Bob",
-//     age: 26,
-//     isMarried: true,
-//     friends: ["Ales", "Bob", "John"]
+// типы данных
+
+// примитивы
+// number, string, boolean, null, undefined, NaN, Symbol, BigInt
+
+// объекты
+// object, array, function
+
+// отличия объектов от примитивов
+// - более сложная структура
+// - имеют свойства и методы
+// - ссылочный тип данных
+
+const user1 = { // new Object(); в переменной user1 хранится ссылка на объект
+    name: 'Bob',
+    age: 24,
+    isStudent: false
+} // литерал объекта
+
+// const user2 = user1
+//
+// user2.name = 'Alex'
+// console.log(user1.name)
+
+// const user1Copy = {
+//     name: user1.name,
+//     age: user1.age,
+//     isStudent: user1.isStudent
 // }
+//
+// console.log(user1 === user1Copy)
 
-// const arr = [1,2,3,4,5];
-//
-// const copyUser = user;  // #4567 - тот же адрес
-// не происходит создание объекта, нет {}
-//  new Object() - вызов ф-ции контсруктора
-//  Object.assign({}, user) - эквивалент {...user} - старомодно
-//  [] - new Array(...0)
-//
-// user.name = "Anna";
-// console.log(copyUser.name);
-//
-// const realCopyUser = {...user} // #5666
-//  realCopyUser.name = user.name;
-//  realCopyUser.age = user.age;
-//  realCopyUser.isMarried = user.isMarried;
-//
-// const realCopyArr = [...arr];
-//
-// console.log(realCopyUser);
-// console.log(realCopyUser === user); // false - лежат в разных местах памяти(разные ссылки)
-// console.log(copyUser === user);
-//
-// realCopyUser.name = "Bob";
-// console.log(user.name);
+const user1Copy = {...user1}
 
-const user = {  // #4567 (адрес объекта в ОПБ)
-    name: "Bob",
-    age: 26,
-    isMarried: true,
-    friends: ["Ales", "Bob", "John"],
-    address: {
-        city: "Bobruisk",
-        street: "Lenina"
-    }
-}
+user1Copy.name = 'Alex'
+console.log(user1)
+console.log(user1 === user1Copy) // true or false
 
-const copyUser = {...user};
-copyUser.friends.push("Ann");
-console.log(user);
-console.log(user === copyUser); // false - разные объекты, оба раза созданы объекты {}
-console.log(user.friends === copyUser.friends); // true!!!!
-
-// const deepCopy = {...user, friends: [...user.friends]}
-const deepCopy = {...user, friends: [...user.friends], address: {...user.address}};
-console.log(deepCopy);
-console.log(deepCopy === user);
-console.log(deepCopy.address === user.address);
-
-const students = [
-    {name: "Bob"},
-    {name: "Alex"},
-    {name: "Ann"}
+const users = [
+    {
+        id: 1,
+        name: 'Bob',
+        isStudent: true
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        isStudent: true
+    },
+    {
+        id: 3,
+        name: 'Ann',
+        isStudent: true
+    },
+    {
+        id: 4,
+        name: 'Donald',
+        isStudent: true
+    },
 ]
 
-const copyStudents = [...students];
+// изменения данных в реакте производим иммутабельно
+// создаем копию -> вносим изменения в копию -> изпользуем копию с изменениями
 
-// const deepCopyStudents = [];
-// for (let i = 0; i < students.length; i++) {
-//     deepCopyStudents[i] = {...students[i]};
-// }
+const usersCopy = [...users]
+usersCopy.pop()
 
-const deepCopyStudents = students.map(student => {
-    return {...student}
-});
+console.log(users)
+console.log(usersCopy)
 
+console.log(users === usersCopy) // false -> react увидит изменения и запустит перерисовку
+
+// C(R)UD
+
+// создание(добавление) нового элемента
+
+const newUser = {
+    id: 5,
+    name: 'John',
+    isStudent: true
+}
+
+const addUsers = [...users, newUser]
+
+// выборочное изменение элемента
+const upd1Users = addUsers.map(user => user.name === 'John'? {...user, isStudent: false} : user)
+console.log(upd1Users)
+
+// изменение всех элементов
+
+const upd2Users = upd1Users.map(user => ({...user, isMarried: true}))
+console.log(upd2Users)
+
+// удаление элемента
+
+const deleteUsers = upd2Users.filter(user => user.id !== 5)
+console.log(deleteUsers)
+console.log(deleteUsers === upd2Users)
