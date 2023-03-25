a6f96969fc6ce1ccf5d414b97cabb6f8cdeb57d6
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-25 21:19:54 +0300

    Create todoLists array

---
+++ lesson-4/main.js
@@ -0,0 +1,22 @@
+const todoLists = [
+    {
+        id: '23hf-76jh',
+        title: 'What to learn',
+        filter: 'all',
+        tasks: [
+            {id: 1, title: 'HTML', isDone: false},
+            {id: 2, title: 'CSS', isDone: false},
+            {id: 3, title: 'JS', isDone: false},
+        ]
+    },
+    {
+        id: '98ik-ds',
+        title: 'What to buy',
+        filter: 'all',
+        tasks: [
+            {id: 4, title: 'Bread', isDone: false},
+            {id: 5, title: 'Milk', isDone: false},
+            {id: 6, title: 'Chicken', isDone: false},
+        ]
+    },
+]
