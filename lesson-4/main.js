5363a0877f76e30c2f49566fb71e1ee57c07d83a
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-25 21:32:54 +0300

    Refactor tasks array with ids as keys

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -25,12 +25,12 @@ const todoLists = [
 ]
 
 const tasks = {
-    todoListId1: [
+    [todoListId1]: [
         {id: 1, title: 'HTML', isDone: false},
         {id: 2, title: 'CSS', isDone: false},
         {id: 3, title: 'JS', isDone: false},
     ],
-    todoListId2: [
+    [todoListId2]: [
         {id: 4, title: 'Bread', isDone: false},
         {id: 5, title: 'Milk', isDone: false},
         {id: 6, title: 'Chicken', isDone: false},
