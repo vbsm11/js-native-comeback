5c3cda6e304897752f1344f07a903a8ad80f9528
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-26 22:30:42 +0300

    Reduce for sum elements of array

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -25,17 +25,28 @@ const todoLists = [
 ]
 
 const tasks = {
-    [todoListId1]: [
+    [todoListId1]: [ // '23hf-76jh'
         {id: 1, title: 'HTML', isDone: false},
         {id: 2, title: 'CSS', isDone: false},
         {id: 3, title: 'JS', isDone: false},
     ],
-    [todoListId2]: [
+    [todoListId2]: [ // '98ik-ds'
         {id: 4, title: 'Bread', isDone: false},
         {id: 5, title: 'Milk', isDone: false},
         {id: 6, title: 'Chicken', isDone: false},
-    ]
+    ],
+    // [12+38]: ... === '50': ...
 }
 
 // console.log(tasks[todoLists[0].title])
 // console.log(tasks[todoLists[0].title].filter(t => t.id !== 1))
+
+console.log(tasks[todoListId1][1].title) // 'CSS'
+
+// reduce - массив => одно значение
+
+const nums = [2, 4, 6, 3, 5]
+
+console.log(nums.reduce((acc, el) => {
+    return acc + el
+}, 0))
