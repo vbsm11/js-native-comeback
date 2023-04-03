f196a297d294386906f8d96dba672c78bd4149e2
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-03 21:40:00 +0300

    How works sort with numbers

--- lesson-5/main.js
+++ lesson-5/main.js
@@ -15,3 +15,29 @@ console.log(names)
 // 4. Возвращает ссылку на исходный массив
 
 console.log(names.sort() === names) // true
+
+// 5. По умолчанию сортирует числа по первой цифре (тот же unicode)
+// для реальной сортировки чисел нужно передать параметром функцию сравнения (callback)
+
+const nums = [1000, 333, 9, 77, -3]
+console.log(nums.sort())
+
+// 6. Ф-ция сравнения должна вернуть число > или < 0
+
+const compFunc = (a, b) => { // по возрастанию
+  if (a > b) { // надо поменять местами
+      return 10
+  }
+  else {
+      return -1
+  }
+}
+
+console.log(nums.sort(compFunc))
+
+console.log(nums.sort((a, b) => a - b))
+console.log(nums.sort((a, b) => b - a))
+
+// С методом sort часто используют метод reverse
+
+console.log(nums.reverse())
