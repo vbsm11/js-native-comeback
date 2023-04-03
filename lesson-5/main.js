7530a75ef53846822eef41c9fadba50921e349ad
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-03 21:17:19 +0300

    Another 3 paragraphs about how works sort with strings

--- lesson-5/main.js
+++ lesson-5/main.js
@@ -1,2 +1,17 @@
+// 1. Сортирует строки "из коробки", то есть без дополнительных параметров
+
 const names = ['Donald', 'Alex', 'Bob']
 console.log(names.sort())
+
+// 2. Сортирует строки типа по "алфавиту" (согласно unicode)
+
+const names2 = ['Donald', 'alex', 'aLEX', 'игорь', 'Юрий']
+console.log(names2.sort())
+
+// 3. Работает мутабельно (сортирует массив на месте)
+
+console.log(names)
+
+// 4. Возвращает ссылку на исходный массив
+
+console.log(names.sort() === names) // true
