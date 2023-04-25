86954529e3f86afb64a69851ec43d6b0d873d016
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-25 22:26:25 +0300

    getTriangleType

--- lesson-8/lesson_8.ts
+++ lesson-8/lesson_8.ts
@@ -16,9 +16,10 @@ export function sum(...nums: Array<number>): number {
 //  - "00", если такого треугольника не существует.
 
 export function getTriangleType(a: number,b: number,c: number): string {
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return ""
+    if (a > (b + c) || b > (a + c) || c > (a + b)) return '00';
+    if (a === b && b === c) return '10';
+    if (a === b || a === c || b === c) return '01';
+    return "11"
 }
 
 
