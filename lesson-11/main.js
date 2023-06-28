f6ff6d73b609985894c0f181b3c31ffbeb1729d4
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-06-28 07:27:31 +0300

    promise with fetch doesn't work

--- lesson-11/main.js
+++ lesson-11/main.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 const server = {
     getData() {
         return new Promise((res, rej) => {
@@ -13,8 +15,12 @@ const pr = server.getData()
 console.log(pr)
 
 pr.then((data) => { // каждый promise.then при отработке resolve возвращает новый promise (можем создавать цепочки промисов)
-    console.log(data)
+    console.log('data', data)
+    return fetch('https://google.com/?query=js')
 })
+    .then((dataFromGoogle) => {
+        console.log('dataFromGoogle', dataFromGoogle)
+    })
 pr.catch((err) => {
     console.log('ERROR', err)
 }) // catch срабатывает только при срабатывании rejected (при ошибке)
